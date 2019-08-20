fixed variance indexing

compress.py
@@ -6,8 +6,6 @@ from skimage.measure._structural_similarity import compare_ssim as ssim
 
 from tqdm import tqdm
 import argparse
-import random
-import math
 
 import imageio
 import array
@@ -17,7 +15,6 @@ import time
 import os
 
 
-SAMPLE_AREA = 256
 TABLE, QUALITY, SAMPLE_RATIO = QUANTIZATIONTABLE, 64, 1.0
 
 
@@ -25,41 +22,35 @@ def jpeg(partition):
     return capture(zig_zag(quantize(dct_2d(partition), table=TABLE)), values=QUALITY, sample_percentage=SAMPLE_RATIO)
 
 
-def SSIM(photo, photo_x, photo_y, area=200, table=QUANTIZATIONTABLE, resample=False):
-    if resample: print(); print("Resampling with new area, previous patch was bad")
-    assert photo_x >= 64 or photo_y >= 64, "Photo too small to run SSIM metric, compression diverges"
-    assert area % 8 == 0, "Invalid sampling area make sure sample area is equally divisible by 8"
-    grab_x, grab_y = int(photo_x // random.uniform(1.5, 4)), int(photo_y // random.uniform(1.5, 4))
-    original_sample = np.array(photo[grab_x:grab_x + area, grab_y:grab_y + area], dtype=np.int16)
-    pbar = tqdm(range(8, 64))
+def SSIM(patch, table=QUANTIZATIONTABLE, resample=False):
+    if resample: print(); print("Resampling with new quantization table")
+    assert patch.shape[0] % 8 == 0 and patch.shape[1] % 8 == 0, \
+        "Invalid sampling area make sure sample area is equally divisible by 8"
+    pbar = tqdm(range(2, 64), desc="Running SSIM metric quality, 2 through 64 sampled weights")
     last_metric, rep = 0, 0
     for i in pbar:
         compressed_data, partitions = array.array('b', []), []
         ext = compressed_data.extend; app = partitions.append
-        pbar.set_description("Running SSIM metric quality, 8 through 64 sampled weights")
-        list_of_patches = split((original_sample.copy() - 128).astype(np.int8), 8, 8)
+        list_of_patches = split((patch.copy() - 128).astype(np.int8), 8, 8)
         [ext(capture(zig_zag(quantize(dct_2d(x), table=table)), values=i)) for x in list_of_patches]
         compressed_split = [compressed_data[z:z + i] for z in range(0, len(compressed_data), i)]
         [app(idct_2d(undo_quantize(zig_zag_reverse(rebuild(y)), table=table)) + 128) for y in compressed_split]
-        index = merge_blocks(partitions, int(area/8), int(area/8)).astype(np.uint8)
-        metric = ssim(original_sample, index, data_range=index.max() - index.min())
-        if math.isnan(metric): return SSIM(photo, photo_x, photo_y, area=area, resample=True)
-        if metric < 0.7:
-            return SSIM(photo, photo_x, photo_y, area=area, table=np.round(table/1.1), resample=True)
-        if metric > 0.98:
+        index = merge_blocks(partitions, int(1), int(1)).astype(np.uint8)
+        metric = ssim(patch, index, data_range=index.max() - index.min())
+        if metric > 0.96:
             if table[0][0] < 8: table[0][0] = 8
             if i % 2 != 0: i += 1
             return i, metric, table
         if abs(last_metric - metric) < 0.0000000001:
-            if metric > 0.955:
+            if metric > 0.94:
                 if table[0][0] < 8: table[0][0] = 8
                 if i % 2 != 0: i += 1
                 return i - rep, metric, table
-            return SSIM(photo, photo_x, photo_y, area=area, table=np.round(table/1.1), resample=True)
+            return SSIM(patch, table=np.round(table/1.1), resample=True)
         rep += 1
         if rep == 4: last_metric = metric; rep = 0
-    if metric < 0.955:
-        return SSIM(photo, photo_x, photo_y, area=area, table=np.round(table/1.2), resample=True)
+    if metric < 0.92:
+        return SSIM(patch, table=np.round(table/1.2), resample=True)
     if table[0][0] < 8: table[0][0] = 8
     return 64, metric, table
 
@@ -88,9 +79,13 @@ if __name__ == '__main__':
     image = imageio.imread(image_path)
     length, width = image[:, :, 0].shape; c_l, c_w, p_l, p_w = precompression_factors(image)
     y, cb, cr = convert_sample(image, length, width)
-    values_to_keep, metric, quant = SSIM(y, p_l, p_w, area=SAMPLE_AREA, table=QUANTIZATIONTABLE)
+    test_y = matrix_multiple_of_eight(y)
+    splits = np.array([np.array(test_y[x:x + 8, z:z + 8], dtype=np.int16) for x in range(0, test_y.shape[0], 8)
+                       for z in range(0, test_y.shape[1], 8)], dtype=np.int16)
+    variances = [np.var(i) for i in splits]
+    original_sample = splits[np.argmax(variances)]
+    values_to_keep, metric, quant = SSIM(original_sample, table=QUANTIZATIONTABLE)
     print('Number of samples (out of 64) to keep at metric ' + str(metric) + ': ', values_to_keep)
-
     keep = [values_to_keep]; padding = [p_l - c_l, p_w - c_w]
     dimensions = convertBin(p_l, bits=16) + convertBin(p_w, bits=16)
     p_length = [convertInt(dimensions[:8], bits=8), convertInt(dimensions[8:16], bits=8)]

compressfolder.py
@@ -8,91 +8,66 @@ import imageio
 from tqdm import tqdm
 import argparse
 
-import random
+from multiprocessing import Pool
+
 import array
-import math
 import time
 import os
 
 
+TABLE, QUALITY, SAMPLE_RATIO = QUANTIZATIONTABLE, 64, 1.0
 
-def compress_image(image, file_name):
 
-    def compress(image, qual=64, sampratio=1.0, table=QUANTIZATIONTABLE):
-        image_copy = image.copy().astype(np.int16)
-        compressed_data = array.array('b', [])
-        ext = compressed_data.extend
-        list_of_patches = split((matrix_multiple_of_eight(image_copy - 128)).astype(np.int8), 8, 8)
-        for x in list_of_patches:
-            ext((capture(zig_zag(quantize(dct_2d(x), table=table)), values=qual, sample_percentage=sampratio)))
-        return compressed_data
+def jpeg(partition):
+    return capture(zig_zag(quantize(dct_2d(partition), table=TABLE)), values=QUALITY, sample_percentage=SAMPLE_RATIO)
 
-    def SSIM(photo, photo_x, photo_y, area=200, table=QUANTIZATIONTABLE):
-        assert photo_x >= 64 or photo_y >= 64, "Photo too small to run SSIM metric, compression diverges"
-        assert area % 8 == 0, "Invalid sampling area make sure sample area is equally divisible by 8"
-        grab_x, grab_y = int(photo_x // random.uniform(1.5, 4)), int(photo_y // random.uniform(1.5, 4))
-        original_sample = np.array(photo[grab_x:grab_x + area, grab_y:grab_y + area], dtype=np.int16)
-        pbar = range(8, 64)
-        last_metric, rep = 0, 0
-        for i in pbar:
-            compressed_data, partitions = array.array('b', []), []
-            list_of_patches = split((original_sample.copy() - 128).astype(np.int8), 8, 8)
-            for x in list_of_patches:
-                comp = capture(zig_zag(quantize(dct_2d(x), table=table)), values=i)
-                compressed_data.extend(comp)
-            compressed_split = [compressed_data[z:z + i] for z in range(0, len(compressed_data), i)]
-            for y in compressed_split:
-                samples = (idct_2d(undo_quantize(zig_zag_reverse(rebuild(y)), table=table)) + 128).astype(np.uint8)
-                partitions.append(samples)
-            index = merge_blocks(partitions, int(area/8), int(area/8)).astype(np.uint8)
-            metric = ssim(original_sample, index, data_range=index.max() - index.min())
-            if math.isnan(metric): return SSIM(photo, photo_x, photo_y, area=area)
-            if metric < 0.7:
-                return SSIM(photo, photo_x, photo_y, area=area, table=np.round(table/1.1))
-            if metric > 0.975: return i, metric, table
-            if abs(last_metric - metric) < 0.0000000001:
-                if metric > 0.93:
-                    return i - rep, metric, table
-                return SSIM(photo, photo_x, photo_y, area=area, table=np.round(table/1.1))
-            rep += 1
-            if rep == 5: last_metric = metric; rep = 0
-        if metric < 0.93:
-            return SSIM(photo, photo_x, photo_y, area=area, table=np.round(table/1.2))
-        return 64, metric, table
 
-    o_length, o_width = image[:, :, 0].shape
-    YCBCR = rgb2ycbcr(image)
-    Y, Cb, Cr = (YCBCR[:, :, 0])[:o_length, :o_width], (YCBCR[:, :, 1])[:o_length, :o_width], \
-                (YCBCR[:, :, 2])[:o_length, :o_width]
+def SSIM(patch, table=QUANTIZATIONTABLE):
+    assert patch.shape[0] % 8 == 0 and patch.shape[1] % 8 == 0, \
+        "Invalid sampling area make sure sample area is equally divisible by 8"
+    pbar = range(2, 64)
+    last_metric, rep = 0, 0
+    for i in pbar:
+        compressed_data, partitions = array.array('b', []), []
+        ext = compressed_data.extend; app = partitions.append
+        list_of_patches = split((patch.copy() - 128).astype(np.int8), 8, 8)
+        [ext(capture(zig_zag(quantize(dct_2d(x), table=table)), values=i)) for x in list_of_patches]
+        compressed_split = [compressed_data[z:z + i] for z in range(0, len(compressed_data), i)]
+        [app(idct_2d(undo_quantize(zig_zag_reverse(rebuild(y)), table=table)) + 128) for y in compressed_split]
+        index = merge_blocks(partitions, int(1), int(1)).astype(np.uint8)
+        metric = ssim(patch, index, data_range=index.max() - index.min())
+        if metric > 0.96:
+            if table[0][0] < 8: table[0][0] = 8
+            if i % 2 != 0: i += 1
+            return i, metric, table
+        if abs(last_metric - metric) < 0.0000000001:
+            if metric > 0.94:
+                if table[0][0] < 8: table[0][0] = 8
+                if i % 2 != 0: i += 1
+                return i - rep, metric, table
+            return SSIM(patch, table=np.round(table/1.1))
+        rep += 1
+        if rep == 4: last_metric = metric; rep = 0
+    if metric < 0.92:
+        return SSIM(patch, table=np.round(table/1.2))
+    if table[0][0] < 8: table[0][0] = 8
+    return 64, metric, table
 
-    c_length, c_width = Y.shape
-    p_length, p_width = calc_matrix_eight_size(Y)
 
-    values_to_keep, metric, quant = SSIM(Cr, p_length, p_width, area=SAMPLE_AREA, table=QUANTIZATIONTABLE)
-    if quant[0][0] < 8: quant[0][0] = 8
-    if values_to_keep % 2 != 0:
-        values_to_keep += 1
+def precompression_factors(image):
+    sample = image[:,:,0]
+    c_length, c_width = sample.shape
+    p_length, p_width = calc_matrix_eight_size(sample)
+    return c_length, c_width, p_length, p_width
 
-    dimensions = convertBin(p_length, bits=16) + convertBin(p_width, bits=16)
-    padding = [p_length - c_length, p_width - c_width]
-    p_length = [convertInt(dimensions[:8], bits=8), convertInt(dimensions[8:16], bits=8)]
-    p_width = [convertInt(dimensions[16:24], bits=8), convertInt(dimensions[24:32], bits=8)]
-    keep = [values_to_keep]
 
-    compressedY = compress(Y, qual=values_to_keep, table=quant)
-    compressedCb = compress(Cb, qual=values_to_keep, sampratio=SAMPLE_RATIO, table=CHROMQUANTIZATIONTABLE)
-    compressedCr = compress(Cr, qual=values_to_keep, sampratio=SAMPLE_RATIO, table=CHROMQUANTIZATIONTABLE)
-
-    q, qc = quant.flatten(), CHROMQUANTIZATIONTABLE.flatten()
-    quantization_tables = array.array('b', q) + array.array('b', qc)
-
-    dim = array.array('b', keep) + array.array('b', p_length) + array.array('b', p_width) + array.array('b', padding)
-    compressed = quantization_tables + dim + compressedY + compressedCb + compressedCr
-    EntropyReduction.bz2(compressed, file_name)
+def convert_sample(img, length, width):
+    YCBCR = rgb2ycbcr(img)
+    y, cb, cr = (YCBCR[:, :, 0])[:length, :width], (YCBCR[:, :, 1])[:length, :width], (YCBCR[:, :, 2])[:length, :width]
+    return y.astype(np.int16), cb.astype(np.int16), cr.astype(np.int16)
 
 
 if __name__ == '__main__':
-    SAMPLE_AREA = 128; SAMPLE_RATIO = 1
     ap = argparse.ArgumentParser()
     ap.add_argument("-i", "--images", required=True,
                     help="image folder")
@@ -100,13 +75,43 @@ if __name__ == '__main__':
                     help="compressed folder")
     args = ap.parse_args()
     start_time = time.time()
-    pbar = tqdm(os.listdir(args.images))
+    pbar = tqdm(os.listdir(args.images), desc="running compressor on all images in folder")
     for file in pbar:
-        pbar.set_description("running compressor on all images in folder")
         filename = os.fsdecode(file)
         resulting_file = args.compressed + os.path.splitext(filename)[0]
         if filename.endswith(".jpg") or filename.endswith(".jpeg"):
             image = imageio.imread(args.images + filename)
-            compress_image(image, resulting_file)
+            length, width = image[:, :, 0].shape; c_l, c_w, p_l, p_w = precompression_factors(image)
+            y, cb, cr = convert_sample(image, length, width)
+            test_y = matrix_multiple_of_eight(y)
+            splits = np.array([np.array(test_y[x:x + 8, z:z + 8], dtype=np.int16) for x in range(0, test_y.shape[0], 8)
+                               for z in range(0, test_y.shape[1], 8)], dtype=np.int16)
+            variances = [np.var(i) for i in splits]
+            original_sample = splits[np.argmax(variances)]
+            values_to_keep, metric, quant = SSIM(original_sample, table=QUANTIZATIONTABLE)
+            keep = [values_to_keep]; padding = [p_l - c_l, p_w - c_w]
+            dimensions = convertBin(p_l, bits=16) + convertBin(p_w, bits=16)
+            p_length = [convertInt(dimensions[:8], bits=8), convertInt(dimensions[8:16], bits=8)]
+            p_width = [convertInt(dimensions[16:24], bits=8), convertInt(dimensions[24:32], bits=8)]
+
+            Y = split((matrix_multiple_of_eight(y - 128)).astype(np.int8), 8, 8)
+            CB = split((matrix_multiple_of_eight(cb - 128)).astype(np.int8), 8, 8)
+            CR = split((matrix_multiple_of_eight(cr - 128)).astype(np.int8), 8, 8)
+
+            QUALITY, TABLE = values_to_keep, quant
+            with Pool(8) as p: compressed_y = array.array('b', np.asarray(p.map(jpeg, Y)).flatten())
+            TABLE = CHROMQUANTIZATIONTABLE
+            with Pool(8) as p:
+                compressed_cb = array.array('b', np.asarray(p.map(jpeg, CB)).flatten())
+                compressed_cr = array.array('b', np.asarray(p.map(jpeg, CR)).flatten())
+
+            q, qc = quant.flatten(), CHROMQUANTIZATIONTABLE.flatten()
+            quantization_tables = array.array('b', q) + array.array('b', qc)
+            dim = array.array('b', keep) + array.array('b', p_length) + array.array('b', p_width) + array.array('b', padding)
+            compressed_data = quantization_tables + dim + compressed_y + compressed_cb + compressed_cr
+
+            size, filename = EntropyReduction.bz2(compressed_data, resulting_file)
+            compressed_file = resulting_file + '.bz2'
+
     print("--- %s seconds ---" % (time.time() - start_time))
 

decompressfolder.py
@@ -1,5 +1,6 @@
 import imageio
 from scipy.ndimage import *
+from multiprocessing import Pool
 
 from tqdm import tqdm
 import argparse
@@ -11,45 +12,23 @@ from jpeg.binutils import convertInt, convertBin
 from compressor.EntropyReduction import EntropyReduction
 
 
-def decompress_image(file_name, image_save):
+SAMPLE_RATIO, TABLE = 1.0, QUANTIZATIONTABLE
 
-    def decompress(input, dimx=0, dimy=0, qual=64, table=QUANTIZATIONTABLE):
-        compressed_split = np.array([np.array(input[i:i+qual], dtype=np.int8)
-                                     for i in range(0, len(input), qual)], dtype=np.int8)
-        image_partitions = []; append = image_partitions.append
-        for x in compressed_split:
-            append((idct_2d(undo_quantize(zig_zag_reverse(rebuild(x)), table=table)) + 128).astype(np.uint8))
-        image = merge_blocks(image_partitions, dimx, dimy)
-        return image
 
-    compressed_bitset = EntropyReduction.bz2_unc(file_name)
-    table = np.asarray(compressed_bitset[:64], dtype=np.float16).reshape(8, 8)
-    tablecr = np.asarray(compressed_bitset[64:128], dtype=np.float16).reshape(8, 8)
-    quality_metric = compressed_bitset[128]
+def jpeg_decompress(partition):
+    return (idct_2d(undo_quantize(zig_zag_reverse(rebuild(partition)), table=TABLE)) + 128).astype(np.uint8)
 
-    p_length = convertInt(convertBin(compressed_bitset[129], bits=8) +
-                          convertBin(compressed_bitset[130], bits=8), bits=16)
-    p_width = convertInt(convertBin(compressed_bitset[131], bits=8) +
-                         convertBin(compressed_bitset[132], bits=8), bits=16)
-    s_length, s_width = int(p_length / 8), int(p_width / 8)
-    length, width = p_length - compressed_bitset[133], p_width - compressed_bitset[134]
 
-    result_bytes = compressed_bitset[135:]
-    no_of_values, no_of_values_cr = int(p_length * p_width / 64 * quality_metric), \
-                                    int(p_length * p_width / 64 * int(quality_metric * SAMPLE_RATIO))
-
-    compressedY = result_bytes[:no_of_values]
-    compressedCb = result_bytes[no_of_values:no_of_values+no_of_values_cr]
-    compressedCr = result_bytes[no_of_values+no_of_values_cr:no_of_values+(2*no_of_values_cr)]
-
-    newY = decompress(compressedY, dimx=s_length, dimy=s_width, qual=quality_metric, table=table)
-    newCb = decompress(compressedCb, dimx=s_length, dimy=s_width, qual=int(quality_metric*SAMPLE_RATIO), table=tablecr)
-    newCr = decompress(compressedCr, dimx=s_length, dimy=s_width, qual=int(quality_metric*SAMPLE_RATIO), table=tablecr)
-
-    YCBCR = np.array([newY[0:length, 0:width], newCb[0:length, 0:width], newCr[0:length, 0:width]]).T
-    rgbArray = ycbcr2rgb(np.flip(YCBCR, axis=1)); rgbArray = rotate(rgbArray, 90)
-
-    imageio.imwrite(image_save, rgbArray, quality=100)
+def image_attributes(compressed):
+    table = np.asarray(compressed[:64], dtype=np.float16).reshape(8, 8)
+    tablecr = np.asarray(compressed[64:128], dtype=np.float16).reshape(8, 8)
+    quality_metric = compressed[128]
+    p_length = convertInt(convertBin(compressed[129],bits=8)+convertBin(compressed[130],bits=8),bits=16)
+    p_width = convertInt(convertBin(compressed[131],bits=8)+convertBin(compressed[132],bits=8),bits=16)
+    length, width = p_length - compressed[133], p_width - compressed[134]
+    val = int(p_length*p_width / 64*int(quality_metric*SAMPLE_RATIO))
+    val_cr = int(p_length*p_width / 64*int(quality_metric*SAMPLE_RATIO))
+    return table, tablecr, p_length, p_width, length, width, val, val_cr
 
 
 if __name__ == '__main__':
@@ -61,11 +40,36 @@ if __name__ == '__main__':
                     help="decompressed folder")
     args = ap.parse_args()
     start_time = time.time()
-    pbar = tqdm(os.listdir(args.compressed))
+    pbar = tqdm(os.listdir(args.compressed), desc="running decompressor on all images in folder")
     for file in pbar:
-        pbar.set_description("running decompressor on all images in folder")
         filename = os.fsdecode(file)
-        resulting_file = args.directory+os.path.splitext(filename)[0] + '.jpg'
+        if args.iden == 'Y' or args.iden == 'y': resulting_file = args.directory+os.path.splitext(filename)[0] + '.jpg'
+        else: image_save = resulting_file = args.directory+os.path.splitext(filename)[0] + '.png'
+
         if filename.endswith(".bz2"):
-            decompress_image(args.compressed+filename, resulting_file)
+            comp = EntropyReduction.bz2_unc(args.compressed+filename)
+
+            data = comp[135:]
+            tab, tabcr, p_length, p_width, length, width, val, val_cr = image_attributes(comp)
+            compressedY, compressedCb, compressedCr = data[:val], data[val:val+val_cr], data[val+val_cr:val+(2*val_cr)]
+
+            TABLE = tab
+            with Pool(8) as p:
+                split_y = [np.array(compressedY[i:i+int(comp[128]*SAMPLE_RATIO)], dtype=np.int8)
+                           for i in range(0, len(compressedY), int(comp[128]*SAMPLE_RATIO))]
+                y = merge_blocks(p.map(jpeg_decompress, split_y), int(p_length / 8), int(p_width / 8))
+            TABLE = tabcr
+            with Pool(8) as p:
+                split_cb = [np.array(compressedCb[i:i+int(comp[128]*SAMPLE_RATIO)], dtype=np.int8)
+                            for i in range(0, len(compressedCb), int(comp[128]*SAMPLE_RATIO))]
+                split_cr = [np.array(compressedCr[i:i+int(comp[128]*SAMPLE_RATIO)], dtype=np.int8)
+                            for i in range(0, len(compressedCr), int(comp[128]*SAMPLE_RATIO))]
+                cb = merge_blocks(p.map(jpeg_decompress, split_cb), int(p_length / 8), int(p_width / 8))
+                cr = merge_blocks(p.map(jpeg_decompress, split_cr), int(p_length / 8), int(p_width / 8))
+
+            YCBCR = np.array([y[0:length, 0:width], cb[0:length, 0:width], cr[0:length, 0:width]]).T
+            rgbArray = ycbcr2rgb(np.flip(YCBCR, axis=1)); rgbArray = rotate(rgbArray, 90)
+
+            imageio.imwrite(resulting_file, rgbArray, quality=100)
+
     print("--- %s seconds ---" % (time.time() - start_time))